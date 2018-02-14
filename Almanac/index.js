d14e67b560d442e5829ff9f68eb167d792ec4c54
Don Drury <don@agruity.com>
2018-02-14 11:39:21 -0600

    left a file out!

--- Almanac/index.js
+++ Almanac/index.js
@@ -1,4 +1,4 @@
-import React, {Component, PropTypes} from 'react'
+import React, {Component} from 'react'
 import AlmanacController from './AlmanacController'
 import SimpleDatePicker from './SimpleDatePicker'
 const ABSOLUTE_HEIGHT = 330;
