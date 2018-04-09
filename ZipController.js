efc844fc699e9aa109a6fd964094c5270f2357a9
mattfrankly <31962021+mattfrankly@users.noreply.github.com>
2018-04-09 18:21:04 -0400

    Update ZipController.js

--- ZipController.js
+++ ZipController.js
@@ -3,8 +3,6 @@ var affiliateAlreadySet = false;
 var affiliate = 'kotv';
 
 function getAffiliate(){
-  console.log("*** ZipController getAffiliate()");
-  console.log("*** ZipController getAffiliate() affiliate ", affiliate);
   return affiliate
 }
 function defaultZip(){
